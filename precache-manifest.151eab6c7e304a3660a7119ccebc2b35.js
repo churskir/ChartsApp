f14b4a527c3254e0e60bfdcafc06ad67e7b93226
churskir <262815@pw.edu.pl>
2019-01-23 09:49:32 +0100

    Updates

---
+++ precache-manifest.151eab6c7e304a3660a7119ccebc2b35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31929eda1894bde6c622",
+    "url": "/ChartsApp/static/js/runtime~main.31929eda.js"
+  },
+  {
+    "revision": "09aeda2420a7b67caf0c",
+    "url": "/ChartsApp/static/js/main.09aeda24.chunk.js"
+  },
+  {
+    "revision": "cc563001b5db291e8109",
+    "url": "/ChartsApp/static/js/1.cc563001.chunk.js"
+  },
+  {
+    "revision": "09aeda2420a7b67caf0c",
+    "url": "/ChartsApp/static/css/main.1623dbcb.chunk.css"
+  },
+  {
+    "revision": "88241f5920623748a3bc66e51ccf24aa",
+    "url": "/ChartsApp/index.html"
+  }
+];
